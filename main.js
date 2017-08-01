64c9658ac3a09b5195a3fb4198ca8cdfce8a9885
Chad Yates <cayates1989@gmail.com>
2017-08-01 19:49:03 -0400

    second commit, made some changes to add customers.id.value in order to get the SSN's to render on the page

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ let html = "";
 for (i=0; i < customers.length; i++){
     var customer = customers[i];
 
-    html += newCustomer(customer.picture.large, customer.name.first, customer.name.last, customer.email, customer.location.street, customer.location.city, customer.location.state, customer.location.postcode, customer.phone, customer.SSN);
+    html += newCustomer(customer.picture.large, customer.name.first, customer.name.last, customer.email, customer.location.street, customer.location.city, customer.location.state, customer.location.postcode, customer.phone, customer.id.value);
 }
 
 document.querySelector('.wrapper').innerHTML = html;
 
 function newCustomer(img, first, last, email, street, city, state, postcode, tel, SSN){
 let post = `
-    <div class = "employee">
+    <div class = "employee">        
         <img src=${img}>
         <h3 class = "name">${first} ${last}</h3>
         <span class = "email"> ${email}</span>
@@ -25,3 +25,4 @@ let post = `
     </div>`;
 return post;
 }
+console.log(data.results);
