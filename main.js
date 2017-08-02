ce705283bf845c5a26434508c09145e9ceead5ff
Chad Yates <cayates1989@gmail.com>
2017-08-01 23:24:02 -0400

    another commit, made some attempts at the masking ssn but its time for bed

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-let customers = data.results;
+let customers = customerinfo.results;
 
-let html = "";
+let pagebody = "";
 
 for (i=0; i < customers.length; i++){
     var customer = customers[i];
 
-    html += newCustomer(customer.picture.large, customer.name.first, customer.name.last, customer.email, customer.location.street, customer.location.city, customer.location.state, customer.location.postcode, customer.phone, customer.id.value);
+    pagebody += newCustomer(customer.picture.large, customer.name.first, customer.name.last, customer.email, customer.location.street, customer.location.city, customer.location.state, customer.location.postcode, customer.phone, customer.id.value);
 }
 
-document.querySelector('.wrapper').innerHTML = html;
+document.querySelector('.wrapper').innerHTML = pagebody;
 
 function newCustomer(img, first, last, email, street, city, state, postcode, tel, SSN){
 let post = `
@@ -19,7 +19,7 @@ let post = `
         <ul class="locinfo">
             <li>${street}</li>
             <li>${city} ${state} ${postcode}</li>
-            <li>${tel}</li>
+            <li>${tel}</li>    
         </ul>
         <div class = "ssn">${SSN}</div>
     </div>`;
